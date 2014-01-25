20ea43e7df450ec1653b393a04f1ea827a24d779
junweizhang23 <junweizhang23@gmail.com>
2014-01-25 01:34:21 -0500

    button click

--- src/com/example/cal/Cal.java
+++ src/com/example/cal/Cal.java
@@ -3,17 +3,63 @@ package com.example.cal;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Cal extends Activity {
 
 	int counter = 0;
 	Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0;
+	Button add, sub, mul, div;
+	EditText result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cal);
+
+		counter = 0;
+
+		b0 = (Button) findViewById(R.id.b0);
+		b1 = (Button) findViewById(R.id.b1);
+		b2 = (Button) findViewById(R.id.b2);
+		b3 = (Button) findViewById(R.id.b3);
+		b4 = (Button) findViewById(R.id.b4);
+		b5 = (Button) findViewById(R.id.b5);
+		b6 = (Button) findViewById(R.id.b6);
+		b7 = (Button) findViewById(R.id.b7);
+		b8 = (Button) findViewById(R.id.b8);
+		b9 = (Button) findViewById(R.id.b9);
+
+		add = (Button) findViewById(R.id.badd);
+		sub = (Button) findViewById(R.id.bsub);
+		mul = (Button) findViewById(R.id.bmul);
+		sub = (Button) findViewById(R.id.bdiv);
+
+		result = (EditText) findViewById(R.id.Result);
+
+		b0.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// counter = counter + 1;
+				
+				 result.setText(counter);
+			}
+		});
+
+		sub.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 	}
 
 	@Override
